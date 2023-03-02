11729aa09db2edd0b4b27dfb572af4a6806c01b0
Alejandro Medina <drolejan@gmail.com>
2023-03-02 08:43:46 -0600

    pruebaGameManager

--- Assets/gameManager.cs
+++ Assets/gameManager.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class PruebaCodigo : MonoBehaviour
+public class gameManager : MonoBehaviour
 {
     // Start is called before the first frame update
     void Start()
